513c378a42363a7d4144fbbf316f7f060e792bbd
kabirraymalik <kabirraymalik@gmail.com>
2024-07-29 20:19:30 -0700

    live sim working with gripper macro for gello trigger compatibility

--- gello_config.py
+++ gello_config.py
@@ -1,6 +1,7 @@
 import dynamixel_utils
 import os
 import numpy as np
+import time
 
 if os.path.exists("/dev/serial"):
     device_name = "/dev/serial/by-id/usb-FTDI_USB__-__Serial_Converter_FT94VY5B-if00-port0"
@@ -9,5 +10,23 @@ else:
 motors = ['XL330', 'XL330', 'XL330', 'XL330', 'XL330', 'XL330', 'XL330']
 
 dm = dynamixel_utils.DynaManager(device_name, motors)
-motor_positions = dm.log_motor_positions()
 offsets = []
+
+#starting motor offsets should be: -pi, pi, -pi/2, 3pi/2, 3pi/2, 2pi
+
+start_angles = [0, 3*np.pi/2, np.pi/2, 3*np.pi/2, 3*np.pi/2, 0]
+
+print('place gello in reference position (trigger open)')
+input = input('ready?\n')
+if input == 'y':
+    motor_positions = dm.log_motor_positions()
+    for i in range(len(start_angles)):
+        offsets.append(start_angles[i] - motor_positions[i])
+    print(f'offsets: {offsets}')
+    in_pi = []
+    for val in offsets:
+        in_pi.append(val/np.pi)
+    print(f'in pi: offsets = {in_pi}')
+    print(f'gripper angle: {dm.get_position(7)}')
+
+
--- live_sim.py
+++ live_sim.py
@@ -2,41 +2,87 @@ import time
 import numpy as np
 import mujoco
 import mujoco.viewer
+import dynamixel_utils
+import os
+import math as m
 
 print(mujoco.__file__)
 
+#dynamixel setup
+if os.path.exists("/dev/serial"):
+    device_name = "/dev/serial/by-id/usb-FTDI_USB__-__Serial_Converter_FT94VY5B-if00-port0"
+else:
+    device_name = "/dev/tty.usbserial-FT94VY5B"
+motors = ['XL330', 'XL330', 'XL330', 'XL330', 'XL330', 'XL330', 'XL330']
 
-m = mujoco.MjModel.from_xml_path('resources/scene.xml')
+dmR = dynamixel_utils.DynaManager(device_name, motors)
+
+#mujoco setup
+m = mujoco.MjModel.from_xml_path('resources/bimanual-ur3/scene.xml')
 d = mujoco.MjData(m)
-motorAngles = []
 
-#this is old
-def key_listener(keycode):
-    #print(keycode)
-    if chr(keycode) == ' ':
-        global paused
-        paused = not paused
-    if chr(keycode) == '256':
-       endSim = True
+#default config for setup, modify to change reference position (test with single arm in sim)
+start_angles = [0, 3*np.pi/2, np.pi/2, 3*np.pi/2, 3*np.pi/2, 0]
+
+#gello info
+motorAngles = []
+offsets = [-1.0334554334554336, 0.9896214896214897, -0.5402930402930404, 1.49995115995116, 0.00012210012210012692, -2.056166056166056]
+joint_orientations = [1, 1, -1, 1, 1, 1]
+gripper_open_pos = 1206
+gripper_range = 550
+
+for id in range(len(offsets)):
+  offsets[id] = offsets[id] * np.pi
+  
 
 def init_controller(model, data):
-   pass
+  data.qpos[0] = start_angles[0]
+  data.qpos[1] = start_angles[1]
+  data.qpos[2] = start_angles[2]
+  data.qpos[3] = start_angles[3]
+  data.qpos[4] = start_angles[4]
+  data.qpos[5] = start_angles[5]
 
 #range of input angles: -pi to pi
 def controller(model, data):
-   data.qpos[0] = 0
-   data.ctrl[1] = 15
-   data.ctrl[2] = 15
+  #gello data recording
 
-   #print output
-   print(f"{data.qpos[0]} | {data.qpos[1]} | {data.qpos[2]} | {data.qpos[3]}")
+  #right arm
+  motor_positions = dmR.log_motor_positions()
+  for motor_id in range(len(motor_positions)-1):
+    if joint_orientations[motor_id] == -1:
+        val = motor_positions[motor_id] + offsets[motor_id]
+        difference = start_angles[motor_id] - val
+        data.qpos[motor_id] = start_angles[motor_id] + difference
+    else:
+      data.qpos[motor_id] = motor_positions[motor_id] + offsets[motor_id]
+  
+  #right hand
+  data.qpos[6] = ability_macro(dmR.get_position(7))
+  data.qpos[7] = ability_macro(dmR.get_position(7))
+  data.qpos[8] = ability_macro(dmR.get_position(7))
+  data.qpos[9] = ability_macro(dmR.get_position(7))
+  data.qpos[10] = ability_macro(dmR.get_position(7))
+  data.qpos[11] = ability_macro(dmR.get_position(7))
+  data.qpos[12] = ability_macro(dmR.get_position(7))
+  data.qpos[13] = ability_macro(dmR.get_position(7))
+  data.qpos[14] = ability_macro(dmR.get_position(7))
+  data.qpos[15] = ability_macro(dmR.get_position(7))
+
+#print output   
+  #print(f"sim thetas: {data.qpos[0]} | {data.qpos[1]} | {data.qpos[2]} | {data.qpos[3]} | {data.qpos[4]} | {data.qpos[5]}")
+   
+def ability_macro(gripper_pos):
+  print(((gripper_open_pos-gripper_pos)/gripper_range))
+  return (np.pi/2)*((gripper_open_pos-gripper_pos)/gripper_range)
+
 
 init_controller(m, d)
 mujoco.set_mjcb_control(controller)
 
 paused = False
 
-with mujoco.viewer.launch_passive(m, d, key_callback=key_listener) as viewer:
+with mujoco.viewer.launch_passive(m, d) as viewer:
   start = time.time()
   while viewer.is_running():
     step_start = time.time()
