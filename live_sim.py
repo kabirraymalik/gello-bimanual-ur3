69faab199ad58e70c79a0b5c2560635cd069caeb
kabirraymalik <kabirraymalik@gmail.com>
2024-08-05 16:33:56 -0700

    joints 5 and 4 giving some issue, everything else fine

--- live_sim.py
+++ live_sim.py
@@ -26,7 +26,8 @@ start_angles = [0, 3*np.pi/2, np.pi/2, 3*np.pi/2, 3*np.pi/2, 0]
 
 #gello info
 motorAngles = []
-offsets = [-1.0334554334554336, 0.9896214896214897, -0.5402930402930404, 1.49995115995116, 0.00012210012210012692, -2.056166056166056]
+#-1.0334554334554336
+offsets = [0, 0.9896214896214897, -0.5402930402930404, 1.49995115995116, 0.00012210012210012692, -2.056166056166056]
 joint_orientations = [1, 1, -1, 1, 1, 1]
 gripper_open_pos = 1206
 gripper_range = 550
@@ -36,44 +37,47 @@ for id in range(len(offsets)):
   
 
 def init_controller(model, data):
-  data.qpos[0] = start_angles[0]
-  data.qpos[1] = start_angles[1]
-  data.qpos[2] = start_angles[2]
-  data.qpos[3] = start_angles[3]
-  data.qpos[4] = start_angles[4]
-  data.qpos[5] = start_angles[5]
+  pass
+  #data.qpos[0] = start_angles[0]
+  #data.qpos[1] = start_angles[1]
+  #data.qpos[2] = start_angles[2]
+  #data.qpos[3] = start_angles[3]
+  #data.qpos[4] = start_angles[4]
+  #data.qpos[5] = start_angles[5]
 
 #range of input angles: -pi to pi
 def controller(model, data):
   #gello data recording
 
   #right arm
+  offset = 16
   motor_positions = dmR.log_motor_positions()
   for motor_id in range(len(motor_positions)-1):
     if joint_orientations[motor_id] == -1:
         val = motor_positions[motor_id] + offsets[motor_id]
         difference = start_angles[motor_id] - val
-        data.qpos[motor_id] = start_angles[motor_id] + difference
+        data.qpos[motor_id + offset] = start_angles[motor_id] + difference
     else:
-      data.qpos[motor_id] = motor_positions[motor_id] + offsets[motor_id]
+      data.qpos[motor_id + offset] = motor_positions[motor_id] + offsets[motor_id]
   
   #right hand
-  data.qpos[6] = ability_macro(dmR.get_position(7))
-  data.qpos[7] = ability_macro(dmR.get_position(7))
-  data.qpos[8] = ability_macro(dmR.get_position(7))
-  data.qpos[9] = ability_macro(dmR.get_position(7))
-  data.qpos[10] = ability_macro(dmR.get_position(7))
-  data.qpos[11] = ability_macro(dmR.get_position(7))
-  data.qpos[12] = ability_macro(dmR.get_position(7))
-  data.qpos[13] = ability_macro(dmR.get_position(7))
-  data.qpos[14] = ability_macro(dmR.get_position(7))
-  data.qpos[15] = ability_macro(dmR.get_position(7))
+  trigger_pos = dmR.get_position(7)
+  data.qpos[21] = ability_macro(trigger_pos)
+  data.qpos[22] = ability_macro(trigger_pos)
+  data.qpos[23] = ability_macro(trigger_pos)
+  data.qpos[24] = ability_macro(trigger_pos)
+  data.qpos[25] = ability_macro(trigger_pos)
+  data.qpos[26] = ability_macro(trigger_pos)
+  data.qpos[27] = ability_macro(trigger_pos)
+  data.qpos[28] = ability_macro(trigger_pos)
+  data.qpos[29] = ability_macro(trigger_pos)
+  data.qpos[30] = ability_macro(trigger_pos)
 
 #print output   
   #print(f"sim thetas: {data.qpos[0]} | {data.qpos[1]} | {data.qpos[2]} | {data.qpos[3]} | {data.qpos[4]} | {data.qpos[5]}")
    
 def ability_macro(gripper_pos):
-  print(((gripper_open_pos-gripper_pos)/gripper_range))
+  #print(((gripper_open_pos-gripper_pos)/gripper_range))
   return (np.pi/2)*((gripper_open_pos-gripper_pos)/gripper_range)
 
 
@@ -87,6 +91,8 @@ with mujoco.viewer.launch_passive(m, d) as viewer:
   while viewer.is_running():
     step_start = time.time()
     if not paused:
+        
+        print(f'position motor 6: {dmR.get_position(6)}')
 
         # mj_step can be replaced with code that also evaluates
         # a policy and applies a control signal before stepping the physics.
